bd69d0268b2f9de03b7b635c5a99dcc2d0706b47
Steven van Blerk <stevenvanblerkrsa@gmail.com>
2024-01-28 14:28:09 +0200

    reordered global values to group them by use

--- src/bespokeSystem/components/GenerativeBlueprint/helpers/globalValues.tsx
+++ src/bespokeSystem/components/GenerativeBlueprint/helpers/globalValues.tsx
@@ -27,24 +27,24 @@ const columnCount = 19; //px
 const gridGapSize = 60; //px
 
 const initialValues: contextData = {
-  isBackgroundGridDisplayed: false,
-  areNodesDisplayed: false,
-  areConnectionPathsDisplayed: true,
-  columnCount,
-  rowCount,
-  nodeCount: 30,
-  stepCount: 10,
-  nodeMaxDisplacement: 3, //gaps
-  AreNodesSynchronised: false, // TO-DO: set initialDelay to 0 when true
-  stepDuration: 3,
-  gridGapSize,
   canvasHeight: rowCount * gridGapSize,
   canvasWidth: columnCount * gridGapSize,
-  frameWidth: 0, //px
   canvasMargin: 25, //px
-  radiusMultiplier: 5,
-  connectionMinLength: 0.5 * gridGapSize,
-  connectionMaxLength: 3.5 * gridGapSize,
+  frameWidth: 0, //px
+  stepDuration: 3,
+  stepCount: 10,
+  isBackgroundGridDisplayed: false,
+  columnCount,
+  rowCount,
+  gridGapSize,
+  areNodesDisplayed: true,
+  AreNodesSynchronised: false, // TO-DO: set initialDelay to 0 when true
+  nodeMaxDisplacement: 3, //gaps
+  nodeCount: 30,
+  radiusMultiplier: 3, //TO-DO: rename to nodeRadiusMultiplier
+  areConnectionPathsDisplayed: true,
+  connectionMinLength: 1 * gridGapSize, // areNodesDisplayed=true---> //0.5 * gridGapSize,
+  connectionMaxLength: 2 * gridGapSize,                               //3.5 * gridGapSize,
 };
 
 export const GlobalValuesContext = createContext<contextData | undefined>(
