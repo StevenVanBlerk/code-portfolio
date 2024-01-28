10c6434d576956857b1979845fe1ed1a8956cc17
Steven van Blerk <stevenvanblerkrsa@gmail.com>
2024-01-28 14:29:44 +0200

    set areNodesDisplayed to false

--- src/bespokeSystem/components/GenerativeBlueprint/helpers/globalValues.tsx
+++ src/bespokeSystem/components/GenerativeBlueprint/helpers/globalValues.tsx
@@ -26,6 +26,8 @@ const rowCount = 10; //px
 const columnCount = 19; //px
 const gridGapSize = 60; //px
 
+// Add controls for these values and presets to quickly switch
+
 const initialValues: contextData = {
   canvasHeight: rowCount * gridGapSize,
   canvasWidth: columnCount * gridGapSize,
@@ -37,14 +39,14 @@ const initialValues: contextData = {
   columnCount,
   rowCount,
   gridGapSize,
-  areNodesDisplayed: true,
+  areNodesDisplayed: false,
   AreNodesSynchronised: false, // TO-DO: set initialDelay to 0 when true
   nodeMaxDisplacement: 3, //gaps
   nodeCount: 30,
   radiusMultiplier: 3, //TO-DO: rename to nodeRadiusMultiplier
   areConnectionPathsDisplayed: true,
-  connectionMinLength: 1 * gridGapSize, // areNodesDisplayed=true---> //0.5 * gridGapSize,
-  connectionMaxLength: 2 * gridGapSize,                               //3.5 * gridGapSize,
+  connectionMinLength: 0.5 * gridGapSize,
+  connectionMaxLength: 3.5 * gridGapSize,
 };
 
 export const GlobalValuesContext = createContext<contextData | undefined>(
