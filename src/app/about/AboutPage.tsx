08ad5c0a9591ff52cb8bcd1adca65202442cb284
Steven van Blerk <stevenvanblerkrsa@gmail.com>
2024-07-22 15:49:10 +0200

    About Me page to-do comments

--- src/app/about/AboutPage.tsx
+++ src/app/about/AboutPage.tsx
@@ -1,5 +1,13 @@
 const AboutPage = () => {
   return <div>Stuff about me</div>;
+  /**
+   * people person soft skills
+   * supporting neurodiversity
+   * Countering imposter syndrome
+   * Collaboration with designers
+   * Collaboration with devs
+   *
+   */
 };
 
 export default AboutPage;
